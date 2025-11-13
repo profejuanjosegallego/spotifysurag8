a16be6b7d65a830d7fcfde6a7a86cd0d347deac7
Posada12 <sposadab12@gmail.com>
2025-11-13 14:48:19 -0500

    feat,se llena diccionario

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,6 +11,56 @@ public class Main {
 
 
         //spotify
+        //necesitamos registrar N cantidad de datos
+        //de espotify, cada cancion debe tener:
+        //id
+        //nombre
+        //artista
+        //duracion
+        //genero
+        //año delanzamiento
+        //clasificacion de edad
+
+        //NECESIDADES (no pasasr de 50 lineas de codigo)
+        Scanner pedirDato = new Scanner(System.in);
+        ArrayList<HashMap<String,Object>>canciones=new ArrayList<>();
+        HashMap<String, Object>cancion=new HashMap<>();
+
+
+        while(true){
+
+            System.out.print("Digita el nombre de la cancion: ");
+            cancion.put("nombre",pedirDato.nextLine());
+
+            System.out.print("Digite el nombre del artista: ");
+            cancion.put("artista", pedirDato.nextLine());
+
+            System.out.print("Digite el genero de la cancion");
+            cancion.put("genero",pedirDato.nextLine());
+
+            System.out.print("Digite la clasificacion de edad");
+            cancion.put("edad",pedirDato.nextLine());
+
+            System.out.print("Digite el año de lanzamiento");
+            cancion.put("año",pedirDato.nextLine());
+
+            System.out.print("Digita el id de la cancion:");
+            cancion.put("id",pedirDato.nextInt());
+
+
+            System.out.print("Digita la duracion de la cancion (minutos)");
+            cancion.put("duracion", pedirDato.nextDouble());
+
+
+
+
+
+
+
+
+
+
+        }
 
 
     }
