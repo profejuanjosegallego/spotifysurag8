f064ac83b1fb07ddd8c63c6001225620570ec27a
Alejandro Meneses <garciamenesesale@gmail.com>
2025-11-13 14:47:14 -0500

    feat se llena diccionario

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,49 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-
         //spotify
+        //Necesitamos registrar N canciones en la base de datos de Spotify,
+        //cada cancion debe tener:
+        //una Id
+        //un nombre
+        //un artista
+        //una duracion
+        //un genero
+        //un año de lanzamiento
+        //una clasificacion de edad
 
+        //Necesidades (Intentar no pasarme de 50 lienas de código)
+        Scanner pedirDato = new Scanner(System.in);
+        ArrayList<HashMap<String, Object>> canciones = new ArrayList<>();
+        HashMap<String, Object>cancion=new HashMap<>();
+        while(true){
+            System.out.print("Digita el nombre de la canción: ");
+            cancion.put("nombre", pedirDato.nextLine());
 
+            System.out.print("Digite el artista de la canción: ");
+            cancion.put("artista", pedirDato.nextLine());
+
+            System.out.print("Digita el genero de la cancion: ");
+            cancion.put("genero", pedirDato.nextLine());
+
+            System.out.print("Digita el año de lanzamiento de la canción: ");
+            cancion.put("añoLanzamiento", pedirDato.nextLine());
+
+            System.out.print("Digite la clasificación de edad que tiene la canción: ");
+            cancion.put("clasificacionEdad", pedirDato.nextLine());
+
+            System.out.print("Digita el Id de la canción: ");
+            cancion.put("id", pedirDato.nextInt());
+
+            System.out.print("Digite la duración de la canción en minutos: ");
+            cancion.put("duracion", pedirDato.nextDouble());
+
+            pedirDato.nextLine();
+        }
     }
 }
