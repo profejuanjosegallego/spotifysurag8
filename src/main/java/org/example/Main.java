a1e0dd2eddf8b1069822e40c0bfe31ea5a204c9f
Aleho16 <perez.alejo.rojas@gmail.com>
2025-11-13 14:47:28 -0500

    feat se llena diccionario

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,6 +11,52 @@ public class Main {
 
 
         //spotify
+        //necesitamos registrar N canciones en la base de datos de
+        //spotify, cada cancion debe tener:
+
+        //id
+        //nombre
+        //artista
+        //duracion
+        //genero
+        //año de lanzamiento
+        //clasificacion de edad
+
+        //necesidades (intentar no pasarme de 50 lineas de codigo)
+        Scanner pedirDato = new Scanner(System.in);
+        ArrayList<HashMap<String,Object>>canciones=new ArrayList<>();
+        HashMap<String,Object>cancion=new HashMap<>();
+        while(true){
+
+            System.out.print("Digita el nombre de la canción: ");
+            cancion.put("nombre", pedirDato.nextLine());
+
+            System.out.print("Digita el artista de la canción: ");
+            cancion.put("artista", pedirDato.nextLine());
+
+            System.out.print("Digita el genero de la canción: ");
+            cancion.put("genero", pedirDato.nextLine());
+
+            System.out.print("Digita el año de lanzamiento de la canción: ");
+            cancion.put("añoLanzamiento", pedirDato.nextLine());
+
+            System.out.print("Digite la clasificación de edad de la canción: ");
+            cancion.put("clasificacion", pedirDato.nextLine());
+
+            System.out.print("Digita el id de la canción: ");
+            cancion.put("id", pedirDato.nextInt());
+
+            System.out.print("Digita la duración de la canción en minutos: ");
+            cancion.put("duracion", pedirDato.nextDouble());
+
+            pedirDato.nextLine();
+
+
+
+
+
+
+        }
 
 
     }
